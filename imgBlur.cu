b255803d0ab74810e96ffb209908c421671c5269
Viraj Patel <viraj_ashok_patel@sfu.ca>
2024-11-19 15:34:41 -0800

    __global__ void blurKernel(): orig code, update

--- imgBlur.cu
+++ imgBlur.cu
@@ -192,7 +192,7 @@ int main(int argc, char *argv[]) {
   {
     printf("iter: %d\n", i);
     //blurKernel<<<dimGrid, dimBlock>>>(deviceOutputImageData, deviceInputImageData, imageWidth, imageHeight);
-    blurKernel<<<1, 768>>>(deviceOutputImageData, deviceInputImageData, imageWidth, imageHeight);
+    blurKernel<<<1, 769>>>(deviceOutputImageData, deviceInputImageData, imageWidth, imageHeight);
   }
   // Transfer data from GPU to CPU
   cudaMemcpy(hostOutputImageData, deviceOutputImageData, imageWidth * imageHeight * sizeof(float), cudaMemcpyDeviceToHost);
