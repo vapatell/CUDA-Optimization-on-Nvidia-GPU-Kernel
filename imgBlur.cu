4d29419bd94e6c33bdbb18f3619b5bc63d9b50b2
Viraj Patel <viraj_ashok_patel@sfu.ca>
2024-11-19 00:09:29 -0800

    Updated imgBlur.cu - inserted code for __global__ void blurKernel()

--- imgBlur.cu
+++ imgBlur.cu
@@ -15,7 +15,34 @@
 
 ///////////////////////////////////////////////////////
 //@@ INSERT YOUR CODE HERE
-__global__ void blurKernel(float *out, float *in, int width, int height) {
+__global__ void blurKernel(float *out, float *in, int width, int height) 
+{
+  int Col = blockIdx.x * blockDim.x + threadIdx.x;
+  int Row = blockIdx.y * blockDim.y + threadIdx.y;
+  int BLUR_SIZE = 2;
+  
+  if (Col < w && Row < h) 
+  {
+    int pixVal = 0; int pixels = 0;
+    // Get the average of the surrounding 2xBLUR_SIZE x 2xBLUR_SIZE box
+    for(int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE+1; ++blurRow) 
+    {
+      for(int blurCol = -BLUR_SIZE; blurCol < BLUR_SIZE+1; ++blurCol) 
+      {
+        int curRow = Row + blurRow;
+        int curCol = Col + blurCol;
+        // Verify we have a valid image pixel
+        if(curRow > -1 && curRow < h && curCol > -1 && curCol < w) 
+        {
+          pixVal += in[curRow * w + curCol];
+          // Keep track of number of pixels in the accumulated total
+          pixels++;
+        }
+      }
+    }
+    // Write our new average pixel value out
+    out[Row * w + Col] = (unsigned char)(pixVal / pixels);
+  }
 }
 ///////////////////////////////////////////////////////
 
@@ -45,7 +72,7 @@ int main(int argc, char *argv[]) {
   imageWidth  = wbImage_getWidth(inputImage);
   imageHeight = wbImage_getHeight(inputImage);
 
-  // Since the image is monochromatic, it only contains only one channel
+  // Since the image is monochromatic, it only contains one channel
   outputImage = wbImage_new(imageWidth, imageHeight, 1);
 
   // Get host input and output image data
@@ -60,26 +87,20 @@ int main(int argc, char *argv[]) {
   //@@ INSERT AND UPDATE YOUR CODE HERE
 
   // Allocate cuda memory for device input and ouput image data
-  cudaMalloc((void **)&deviceInputImageData,
-             imageWidth * imageHeight * sizeof(float));
-  cudaMalloc((void **)&deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(float));
+  cudaMalloc((void **)&deviceInputImageData, imageWidth * imageHeight * sizeof(float));
+  cudaMalloc((void **)&deviceOutputImageData, imageWidth * imageHeight * sizeof(float));
 
   // Transfer data from CPU to GPU
-  cudaMemcpy(deviceInputImageData, hostInputImageData,
-             imageWidth * imageHeight * sizeof(float),
-             cudaMemcpyHostToDevice);
-
+  cudaMemcpy(deviceInputImageData, hostInputImageData, imageWidth * imageHeight * sizeof(float), cudaMemcpyHostToDevice);
+  dim3 dimBlock(16, 16, 1);
+  dim3 dimGrid(ceil(imgWidth/16.0), ceil(imgHeight/16.0), 1);
+  
   // Call your GPU kernel 10 times
   for(int i = 0; i < 10; i++)
-  blurKernel<<<1, 1>>>(deviceOutputImageData,
-                                    deviceInputImageData, imageWidth,
-                                    imageHeight);
+  imgBlurGPU<<<dimGrid, dimBlock>>>(deviceOutputImageData, deviceInputImageData, imageWidth, imageHeight);
 
   // Transfer data from GPU to CPU
-  cudaMemcpy(hostOutputImageData, deviceOutputImageData,
-             imageWidth * imageHeight * sizeof(float),
-             cudaMemcpyDeviceToHost);
+  cudaMemcpy(hostOutputImageData, deviceOutputImageData, imageWidth * imageHeight * sizeof(float), cudaMemcpyDeviceToHost);
   ///////////////////////////////////////////////////////
   
   // Stop and print timer
@@ -91,8 +112,8 @@ int main(int argc, char *argv[]) {
    for(int i=0; i<imageHeight; i++){
      for(int j=0; j<imageWidth; j++){
        if(abs(hostOutputImageData[i*imageWidth+j]-goldOutputImageData[i*imageWidth+j])/goldOutputImageData[i*imageWidth+j]>0.01){
-         printf("Incorrect output image at pixel (%d, %d): goldOutputImage = %f, hostOutputImage = %f\n", i, j, goldOutputImageData[i*imageWidth+j],hostOutputImageData[i*imageWidth+j]);
-	 return -1;
+          printf("Incorrect output image at pixel (%d, %d): goldOutputImage = %f, hostOutputImage = %f\n", i, j, goldOutputImageData[i*imageWidth+j],hostOutputImageData[i*imageWidth+j]);
+	        return -1;
        }
      }
    }
