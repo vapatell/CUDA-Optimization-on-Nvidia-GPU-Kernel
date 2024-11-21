fe789bd72c435f8db65efff94f7818103a977f1b
Viraj Patel <viraj_ashok_patel@sfu.ca>
2024-11-20 21:57:41 -0800

    __global__ void blurKernel(): orig code, update

--- imgBlur.cu
+++ imgBlur.cu
@@ -265,7 +265,7 @@ __global__ void blurKernel(float *out, float *in, int width, int height)
         // Verify we have a valid image pixel
         if(curRow > -1 && curRow < height && curCol > -1 && curCol < width) 
         {
-          pixVal += tile[curRow * width + curCol];
+          //pixVal += tile[curRow * width + curCol];
           pixVal += tile[ty][tx];
           printf("tile[%d][%d] = %f\n", ty, tx, tile[ty][tx]);
           // Keep track of number of pixels in the accumulated total
