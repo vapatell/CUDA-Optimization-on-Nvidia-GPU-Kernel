8cca26d4d0cef0d512e86686583e447b04f0eb4b
Viraj Patel <viraj_ashok_patel@sfu.ca>
2024-11-20 21:59:52 -0800

    __global__ void blurKernel(): orig code, update

--- imgBlur.cu
+++ imgBlur.cu
@@ -246,7 +246,7 @@ __global__ void blurKernel(float *out, float *in, int width, int height)
  __shared__ float tile[BLOCK_DIM][BLOCK_DIM]; // 2D shared memory array
   //float *sharedTile = tile;
   //printf("here0");
-   tile[ty][tx] = in[Row * width + Col];
+   tile[Row][Col] = in[Row * width + Col];
   __syncthreads();
   //printf("tile[%d][%d] = %f\n", ty, tx, tile[ty][tx]);
   if (Col < width && Row < height) 
@@ -266,7 +266,7 @@ __global__ void blurKernel(float *out, float *in, int width, int height)
         if(curRow > -1 && curRow < height && curCol > -1 && curCol < width) 
         {
           //pixVal += tile[curRow * width + curCol];
-          pixVal += tile[ty][tx];
+          pixVal += tile[Row][Col];
           //printf("tile[%d][%d] = %f\n", ty, tx, tile[ty][tx]);
           // Keep track of number of pixels in the accumulated total
           pixels++;
